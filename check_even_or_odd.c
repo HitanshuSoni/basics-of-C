f3a3985a10ab5c3fc43529a422842100f699057e
HitanshuSoni <54149792+HitanshuSoni@users.noreply.github.com>
2019-09-27 20:35:55 +0530

    Create check_even_or_odd.c

---
+++ check_even_or_odd.c
@@ -0,0 +1,26 @@
+Write a C program to check whether a number is even or odd.
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    /* Input number from user */
+    printf("Enter any number to check even or odd: ");
+    scanf("%d", &num);
+    
+    /* Check if the number is divisible by 2 then it is even */
+    if(num % 2 == 0)
+    {
+        /* num % 2 is 0 */
+        printf("Number is Even.");
+    }
+    else
+    {
+        /* num % 2 is 1 */
+        printf("Number is Odd.");
+    }
+
+    return 0;
+}
